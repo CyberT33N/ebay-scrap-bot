2e5587cbf8948b175c5527770d470adfbeee54d0
Dennis <17134633+CyberT33N@users.noreply.github.com>
2020-07-18 04:03:20 +0200

    Add files via upload

---
+++ INSTALL THIS/scrap.js
@@ -0,0 +1,6 @@
+var database = [
+  {"url":"https://www.ebay.de/sch/i.html?_from=R40&_nkw=metall+bett&_sacat=0&LH_TitleDesc=0&Farbe=Schwarz&rt=nc&Liegefl%25C3%25A4che=Doppelbett&_dcat=175758","used":0,"description":"metall bett"},
+  {"url":"https://www.ebay.de/sch/i.html?_from=R40&_nkw=holz+bett&_sacat=0&LH_TitleDesc=0&Farbe=Schwarz&rt=nc&Liegefl%25C3%25A4che=Doppelbett&_dcat=175758","used":0,"description":"holz bett"}
+];
+
+exports.database = database;
